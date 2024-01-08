6cf9aafc0f1940d62f788f3d85335935ed646e3d
Bill Han <billbrownbear23@gmail.com>
2024-01-08 10:50:47 +0900

    Initial Commit

---
+++ EVALabel/EVALabelApp.swift
@@ -0,0 +1,17 @@
+//
+//  EVALabelApp.swift
+//  EVALabel
+//
+//  Created by Bill Han on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct EVALabelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
